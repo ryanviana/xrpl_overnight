3e31ab70c026acc71528effe06a9ff7f8bfa38c7
Ryan Viana <ryanviana@usp.br>
2024-01-21 14:06:38 -0300

    disable eslint

--- packages/nextjs/next.config.js
+++ packages/nextjs/next.config.js
@@ -3,11 +3,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   typescript: {
     ignoreBuildErrors: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
-  },
-  eslint: {
-    ignoreDuringBuilds: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
   },
   webpack: config => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
